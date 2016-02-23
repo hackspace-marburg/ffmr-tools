4489f325a1e829df9bf370f8c9fcc31bbd8e28b4
Oleander Reis <oleander@oleander.cc>
2016-02-23 11:28:54 +0100

    Dirty zone-file generator for nodes.

--- dns-zone-nodes.py
+++ dns-zone-nodes.py
@@ -58,16 +58,16 @@ max_hostname_length = max(map(len, mapped_hostnames))
 print(
 	'''$ORIGIN nodes.marburg.link.
 $TTL 1h
-@{s: <{path_width}} IN SOA          altair.reis.asia. freifunk.hsmr.cc. (
-{s: <{path_width}}                  {serial}      ; serial
-{s: <{path_width}}                  2h              ; Refresh
-{s: <{path_width}}                  30m             ; Retry
-{s: <{path_width}}                  3w              ; Expire
-{s: <{path_width}}                  1h )            ; Negative Cache TTL
+@{s: <{path_width}} IN SOA         altair.reis.asia. freifunk.hsmr.cc. (
+{s: <{path_width}}                 {serial}      ; serial
+{s: <{path_width}}                 2h              ; Refresh
+{s: <{path_width}}                 30m             ; Retry
+{s: <{path_width}}                 3w              ; Expire
+{s: <{path_width}}                 1h )            ; Negative Cache TTL
 
-@{s: <{path_width}} IN NS           altair.reis.asia.
-@{s: <{path_width}} IN NS           wega.reis.asia.
-@{s: <{path_width}} IN NS           arturo.reis.asia.
+@{s: <{path_width}} IN NS          altair.reis.asia.
+@{s: <{path_width}} IN NS          wega.reis.asia.
+@{s: <{path_width}} IN NS          arturo.reis.asia.
 	'''.format(
 		serial=datetime.utcnow().strftime('%Y%m%d%H'),
 		s=' ',
